dd3d51c41f027f72286c391d3a7d3232e45763b8
Kuba <jakub.ozdzynski@gmail.com>
2020-03-07 09:31:50 +0100

    commit1

---
+++ src/patterns/Main.java
@@ -0,0 +1,12 @@
+package patterns;
+
+import patterns.creational.prototype.ShoppingList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    private static void testPrototype() {
+        ShoppingList shoppingListTemplate = new ShoppingList()
+    }
+}
